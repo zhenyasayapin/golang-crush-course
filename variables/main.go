7b577a512da8a394d281f7752aeb2048f79f95d8
Zhenya Sayapin <ep.sayapin@gmail.com>
2025-12-02 19:15:56 +0400

    Guess the number Part I

--- variables/main.go
+++ variables/main.go
@@ -1,10 +1,36 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const prompt = "and press ENTER..."
+
 func main() {
-	var firstNumber int
-	firstNumber = 10
-
+	var firstNumber = 10
 	var secondNumber = 2
+	var subtraction = 7
+	// var answer int
 
-	subtraction := 7
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Guess the Number Game!")
+	fmt.Println("______________________")
+	fmt.Println("")
+	fmt.Println("Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Multiply your number by ", firstNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Nom multiply your result by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now divide your result by your original number", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Finally subtract", subtraction, prompt)
+	reader.ReadString('\n')
 }
