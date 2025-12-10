099d0c247893037d616b938c72c4781f215052d1
Zhenya Sayapin <ep.sayapin@gmail.com>
2025-12-10 21:02:27 +0400

    Add User data structure

--- string-interpolation/main.go
+++ string-interpolation/main.go
@@ -10,13 +10,26 @@ import (
 
 var reader *bufio.Reader
 
+type User struct {
+	Name           string
+	Age            int
+	FavoriteNumber float64
+	OwnsADog       bool
+}
+
 func main() {
 	reader = bufio.NewReader(os.Stdin)
 
-	userName := readString("Enter your name:")
-	age := readInt("How old are you?")
+	var user User
 
-	fmt.Printf("Your name is %s. You are %d years old.\n", userName, age)
+	user.Name = readString("Enter your name:")
+	user.Age = readInt("How old are you?")
+	user.FavoriteNumber = readFloat("What is your favorite number?")
+
+	fmt.Printf("Your name is %s. You are %d years old. Your favorite number is %.2f.\n",
+		user.Name,
+		user.Age,
+		user.FavoriteNumber)
 }
 
 func prompt() {
@@ -51,6 +64,23 @@ func readInt(s string) int {
 	if err != nil {
 		fmt.Println("Please enter an integer.")
 		return readInt(s)
+	}
+
+	return num
+}
+
+func readFloat(s string) float64 {
+	fmt.Println(s)
+	prompt()
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput, "\r\n", "", -1)
+	userInput = strings.Replace(userInput, "\n", "", -1)
+
+	num, err := strconv.ParseFloat(userInput, 64)
+
+	if err != nil {
+		fmt.Println("Please enter a floating point number.")
+		return readFloat(s)
 	}
 
 	return num
